1934e5b2063125bdd01bd81633f7e75454c483db
Peter Gonzalez <ptr.jgz@gmail.com>
2024-09-21 21:46:59 -0400

    Generate map and sample markers

--- app/index.js
+++ app/index.js
@@ -1,61 +1,47 @@
-import React, { useState, useEffect } from "react";
-import { Text, View } from "react-native";
+import React from "react";
+import { View, Image } from "react-native";
 import MapView, { Marker } from "react-native-maps";
-
-import * as Location from "expo-location";
+import { StyleSheet } from "react-native";
 
 export default function Index() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
-
-  useEffect(() => {
-    (async () => {
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== "granted") {
-        setErrorMsg("Permission to access location was denied");
-        return;
-      }
-
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
-
-  let text = "Waiting..";
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
-
-  console.log(location);
-
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={styles.container}>
       <MapView
-        style={{ width: "100%", height: "100%" }}
-        initialRegion={
-          {
-            // latitude: location.coords.latitude,
-            // latitude: location.coords.longitude,
-            // latitudeDelta: 0.0922,
-            // longitudeDelta: 0.0421,
-          }
-        }
+        style={styles.map}
+        initialRegion={{
+          latitude: 25.765652,
+          longitude: -80.194665,
+          latitudeDelta: 0.00922,
+          longitudeDelta: 0.00421,
+        }}
       >
-        {/* <Marker
-          coordinate={{
-            latitude: location.coords.latitude,
-            longitude: location.coords.longitude,
-          }}
-        /> */}
+        <Marker
+          coordinate={{ latitude: 25.765652, longitude: -80.194665 }}
+          title="Me"
+        />
+        <Marker
+          coordinate={{ latitude: 25.767652, longitude: -80.194665 }}
+          title="Photographer 1"
+        ></Marker>
+        <Marker
+          coordinate={{ latitude: 25.763652, longitude: -80.192665 }}
+          title="Photographer 2"
+        />
+        <Marker
+          coordinate={{ latitude: 25.763652, longitude: -80.196665 }}
+          title="Photographer 3"
+        />
       </MapView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  map: {
+    width: "100%",
+    height: "100%",
+  },
+});
